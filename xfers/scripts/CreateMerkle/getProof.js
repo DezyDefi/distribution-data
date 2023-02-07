18c66051e5c2d57e92ed67494ac432bc3890a16c
silvercondor <biocrypto@protonmail.com>
2023-02-07 02:23:27 +0000

    fix: fix data

--- xfers/scripts/CreateMerkle/getProof.js
+++ xfers/scripts/CreateMerkle/getProof.js
@@ -5,23 +5,12 @@ const fs = require("fs")
 const tree = StandardMerkleTree.load(JSON.parse(fs.readFileSync("tree.json")));
 
 // (2)
-let out={}
 for (const [i, v] of tree.entries()) {
-  // if (v[0] === '0x15f8F85c0B22cc5614E7eb353fA69AedC166d579') {
-  //   // (3)
-  //   const proof = tree.getProof(i);
-  //   console.log('root:',tree.root)
-  //   console.log('Value:', v);
-  //   console.log('Proof:', proof);
-  // }
-    //   const proof = tree.getProof(i);
-  //   console.log('root:',tree.root)
-    let proof=tree.getProof(i)
+  if (v[0] === '0x15f8F85c0B22cc5614E7eb353fA69AedC166d579') {
+    // (3)
+    const proof = tree.getProof(i);
+    console.log('root:',tree.root)
     console.log('Value:', v);
     console.log('Proof:', proof);
-    out[v[0]]={
-      proof:proof,
-      values:v
-    }
+  }
 }
-fs.writeFileSync("out.json", JSON.stringify(out));
